38bef766a55852b982ace16fb7ea21f91d019c44
Jeremy Grosser <jeremy@synack.me>
2022-03-23 14:17:41 -0700

    remove debug print

--- test/src/synack_misc_test.adb
+++ test/src/synack_misc_test.adb
@@ -1,6 +1,4 @@
 with Ada.Assertions; use Ada.Assertions;
-with Ada.Text_IO; use Ada.Text_IO;
-with Text_Format; use Text_Format;
 with HAL; use HAL;
 with Checksum;
 
@@ -11,7 +9,5 @@ begin
       (Data    => UInt8_Array'(16#BE#, 16#EF#),
        Poly    => 16#31#,
        Initial => 16#FF#);
-   Put_Line (Hex (Sum));
-
    Assert (Sum = 16#92#);
 end Synack_Misc_Test;
