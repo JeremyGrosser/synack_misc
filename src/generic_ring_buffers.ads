a958c2baf240dfed46c60f88d314459a9d09cbba
Jeremy Grosser <jeremy@synack.me>
2022-04-09 17:33:11 -0700

    copyright headers

--- src/generic_ring_buffers.ads
+++ src/generic_ring_buffers.ads
@@ -1,3 +1,8 @@
+--
+--  Copyright (C) 2022 Jeremy Grosser <jeremy@synack.me>
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
 generic
    Capacity : Positive := 1;
    type Element_Type is private;
